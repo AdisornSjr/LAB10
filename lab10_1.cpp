185d1fa3635ada596d63236fcbef8cde70c497c2
Fluke <adisorn.sjr@gmail.com>
2022-12-27 16:24:39 +0700

    lol

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan, interest, pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +24,27 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int sum,i = 0;
+	while(loan>0){
+			
+			cout << fixed << setprecision(2); 
+			cout << setw(13) << left <<	i+1; 
+			cout << setw(13) << left << loan;
+			double Interest = loan*interest/100;
+			cout << setw(13) << left << Interest;
+			double Total = loan+Interest;
+			cout << setw(13) << left << Total;
+			if(pay > Total){
+				pay = Total;
+			}
+			cout << setw(13) << left << pay;
+			double NewBalance = Total-pay;
+			cout << setw(13) << left << NewBalance;
+			cout << "\n";
+			loan = NewBalance;
+			i++;
+			}
+
 	
 	return 0;
 }
